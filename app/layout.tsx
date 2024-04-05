471c561cfaeae4c0176803f8591e1959e0f38b14
Maricarl dennis <maridennis@Maricarls-MBP.attlocal.net>
2024-04-05 13:51:11 -0700

    next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+ 
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+         <body className={`{inter.className} antialised`}>{children}</body>
     </html>
   );
 }
+
+//Adding inter to the <body> element, the font will be applied throughout the application.
+//antialised is  a Tailwind class which smooths out the font. Not necessary but nice touch
